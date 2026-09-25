594315338ce690c32de4de5a27e346fa64b4d09a
Neeraj <neerajkrishna500@gmail.com>
2026-09-24 18:44:55 -0600

    Lesson 3

---
+++ lesson3/a1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter A Random Number"))
+b=int(input("Enter A Different Number"))
+print(a+b)
+marks=int(input("Enter a randoom number"))
+if(marks>75):
+ print("GOOD JOB")
+elif(marks>50):
+    print("CONSIDERABLE")
+
+else:
+    print("WORK HARDER NEXT TIME")
+    
